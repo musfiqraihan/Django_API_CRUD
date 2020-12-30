1405fc867a797580ae0d3753fc6f09914d008d3d
MusfiqRaihan <musfiqak@gmail.com>
2020-12-30 17:13:40 +0600

    short api

--- API_CRUD/urls.py
+++ API_CRUD/urls.py
@@ -4,8 +4,31 @@ from CRUD import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi/', views.student_api),
-    path('studentapi/<int:pk>', views.student_api),
+    path('studentapi/', views.LCStudentAPI.as_view()),
+    path('studentapi/<int:pk>', views.RUDStudentAPI.as_view()),
+
+
+
+
+
+
+    # path('studentapi/', views.StudentList.as_view()),
+    # # path('studentapi/', views.StudentCreate.as_view()),
+    # # path('studentapi/<int:pk>', views.StudentRetrive.as_view()),
+    # # path('studentapi/<int:pk>', views.StudentUpdate.as_view()),
+    # path('studentapi/<int:pk>', views.StudentDelete.as_view()),
+
+
+
+
+
+    # path('studentapi/', views.StudentAPI.as_view()),
+    # path('studentapi/<int:pk>', views.StudentAPI.as_view()),
+
+
+    # path('studentapi/', views.student_api),
+    # path('studentapi/<int:pk>', views.student_api),
+
     # path('studentapi/', views.StudentAPI.as_view()),
     # path('student/', views.hello_world),
 ]
